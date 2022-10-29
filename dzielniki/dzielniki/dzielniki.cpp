0d058a0d2b7a498e492d234e08fa9bf19d38bd26
przeros <s180150@student.pg.edu.pl>
2022-10-29 21:43:57 +0200

    added dzielniki project

---
+++ dzielniki/dzielniki/dzielniki.cpp
@@ -0,0 +1,49 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int* wyznaczDzielniki(int liczba)
+{
+    int* dzielniki = new int[liczba];
+    int licznik = 0;
+    for (int i = 0; i < liczba; i++) {
+        dzielniki[i] = 0;
+    }
+
+    for (int i = 1; i <= liczba; i++) {
+        if (liczba % i == 0) {
+            dzielniki[licznik] = i;
+            licznik++;
+        }
+    }
+
+    return dzielniki;
+}
+
+void wypiszSume(int* dzielniki)
+{
+    int suma = 0;
+    for (int i = 0; dzielniki[i] != 0; i++)
+    {
+        suma += dzielniki[i];
+        if (i == 0)
+        {
+            cout << dzielniki[i];
+        }
+        else
+        {
+            cout << "+" << dzielniki[i];
+        }
+    }
+    cout << "=" << suma;
+}
+
+int main() {
+    int suma = 0;
+    int liczba;
+    cout << "Podaj liczbe: ";
+    cin >> liczba;
+    wypiszSume(wyznaczDzielniki(liczba));
+
+    return 0;
+}
