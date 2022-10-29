89b3db0b82bf80aa644138a7be96c4f7339ac090
przeros <s180150@student.pg.edu.pl>
2022-10-29 22:03:31 +0200

    added czynniki project

---
+++ czynniki/czynniki/czynniki.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main() {
+	int liczba, dzielnik, counter;
+	cin >> liczba;
+	counter = 0;
+	dzielnik = 2;
+	while (true) {
+		if (liczba % dzielnik == 0) {
+			liczba = liczba / dzielnik;
+			counter += 1;
+		}
+		else {
+			if (liczba == 1) {
+				cout << dzielnik << "^" << counter;
+				break;
+			}
+			if (counter != 0)
+				cout << dzielnik << "^" << counter << " * ";
+			dzielnik += 1;
+			counter = 0;
+		}
+	}
+
+	return 0;
+}
